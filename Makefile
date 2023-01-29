a4826428529cfd7753dbc6da4cd6c39907bd2416
Bufo <bufo24@users.noreply.github.com>
2023-01-29 11:09:49 +0200

    start on CI with github actions

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
+prepare:
+	rustup target add wasm32-unknown-unknown
+
 build:
 	cargo build
 
 run: build
 	./target/debug/merkle-tree
+
+test: build
+	cargo test
--- tests/main.rs
+++ tests/main.rs
@@ -11,11 +11,14 @@ mod tests {
 
     #[test]
     fn insert_left() {
-        let tree =
-            MerkleTree::new("root".to_string()).set_left_child(MerkleTree::new("left".to_string()));
+        let tree = MerkleTree::new("root".to_string()).set_left_child("left".to_string());
 
         if let Some(node) = tree.left_child {
             assert_eq!(node.get_value(), "left");
+            assert_eq!(
+                node.get_hash(),
+                "360f84035942243c6a36537ae2f8673485e6c04455a0a85a0db19690f2541480"
+            );
         }
 
         assert!(tree.right_child.is_none());
